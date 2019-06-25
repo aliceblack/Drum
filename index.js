85b769a073029bb4a6ee4b264413e3ddb1a68eda
aliceblack <blackfluid44@gmail.com>
2019-06-25 21:09:10 +0200

    Drum sequence from bot

--- index.js
+++ index.js
@@ -20,3 +20,38 @@ function beat() {
     oscillator.start(audioContext.currentTime);
     oscillator.stop(audioContext.currentTime + 0.5);
 }
+
+function playSteps(steps) {
+    let element = document.getElementById("stepsSequence");
+
+    let i = 0;                     
+    function loop () {       
+       setTimeout(function () { 
+            if (i < steps.length) {
+                element.innerHTML += ' '+steps[i];
+                if(steps[i]){beat();}   
+                loop();
+            }  
+            i++;                  
+       }, 1000)
+    }
+    loop();
+}
+
+function getTrackFromDrumBot() {
+    var init = { method: 'GET',
+               mode: 'cors',
+               cache: 'default' };
+    fetch("https://api.noopschallenge.com/drumbot/patterns/nipnop",init)
+    .then(response => {
+      response.json().then(body=>{
+        console.log(body.tracks[0].steps);
+        playSteps(body.tracks[0].steps);
+      });
+    })
+    .catch(error => console.log(error))
+}
+
+function sequence(){
+    getTrackFromDrumBot();
+}
